8d96cd11370e270a7feeaabcd8d84710701dc3b4
Mariko Sawada <72525778+mariko-sawada@users.noreply.github.com>
2022-08-22 22:52:58 +0900

    Add files via upload

---
+++ MySQL/HR_TypeOfTriangle.sql
@@ -0,0 +1,8 @@
+SELECT
+CASE
+  WHEN (A + B <= C) OR (B + C <= A) OR (A + C <= B) THEN "Not A Triangle"
+  WHEN A = B AND B = C AND A = C THEN "Equilateral"
+  WHEN A <> B AND B <> C AND A <> C THEN "Scalene"
+  ELSE "Isosceles"
+  END AS TRIANGLE_TYPE
+FROM TRIANGLES
